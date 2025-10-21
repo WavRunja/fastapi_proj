fe2c2778c2d54ffda2b1aeaa638fea02955920b7
WavRunja <pereverzew-wladimir@yandex.ru>
2025-10-21 15:27:39 +0300

    Code for Chapter 5

--- Dockerfile
+++ Dockerfile
@@ -5,10 +5,11 @@ WORKDIR /app
 
 # Установим зависимости
 COPY requirements.txt .
+RUN pip install --upgrade pip
 RUN pip install --no-cache-dir -r requirements.txt
 
 # Копируем проект
-COPY . .
+COPY ./app ./app
 
 # Команда запуска (Uvicorn)
 CMD ["uvicorn", "app.main:app", "--host", "0.0.0.0", "--port", "8000"]
--- app/main.py
+++ app/main.py
@@ -1,9 +1,16 @@
 from fastapi import FastAPI
+from app.database import Base, engine
+from app.routers import users
 
+# Создаем таблицы
+Base.metadata.create_all(bind=engine)
 
-app = FastAPI()
+app = FastAPI(title="FastAPI Mini App")
+
+# Подключаем роутеры
+app.include_router(users.router)
 
 
 @app.get("/")
 def root():
-    return {"message": "Hello, FastAPI!"}
+    return {"message": "Добро пожаловать в FastAPI mini app!"}
